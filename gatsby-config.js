8e209bef1d0bc23391bd6b12fc9178a83a015616
Robin <robinbecherraz2@hotmail.fr>
2021-02-12 13:19:49 +0100

    Corrected GA tracking ID

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,7 +73,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "G-J4X0VXKRKS",
+        trackingId: "UA-136950512-2",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: false,
         // Setting this parameter is optional
