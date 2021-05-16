ff6abb775e6bb480a44433ba4e91f038f25787ab
Robin <robinbecherraz2@hotmail.fr>
2021-05-16 23:31:43 +0200

    Added buttons animation

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,11 +7,17 @@ const { useState } = React;
 const Layout = ({ location, children }) => {
   const [menuIsOpen, setMenuIsOpen] = useState(false);
   const [searchIsOpen, setSearchIsOpen] = useState(false);
+  const [showCrossIcon, setShowCrossIcon] = useState(false);
+  const [animation, setAnimation] = useState('');
 
   const toggleMobileMenu = () => {
     setMenuIsOpen(!menuIsOpen);
   };
   const toggleSearch = () => {
+    setAnimation((searchIsOpen) ? 'slideUp' : 'slideDown')
+    setTimeout(() => {
+      setShowCrossIcon(!showCrossIcon)
+    }, (searchIsOpen) ? 990 : 0)
     setSearchIsOpen(!searchIsOpen);
   };
 
@@ -23,9 +29,9 @@ const Layout = ({ location, children }) => {
           <div className="logo-wrapper">
             <Link to="/"><span>G</span><span className="flipped-letter">BLO</span></Link>
           </div>
-          <button className={`toggle-search-btn ${(searchIsOpen) ? "opened" : ""}`} onClick={toggleSearch}>
-            <svg className={`search-icon ${(searchIsOpen) ? "hidden" : ""}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M23.809 21.646l-6.205-6.205c1.167-1.605 1.857-3.579 1.857-5.711 0-5.365-4.365-9.73-9.731-9.73-5.365 0-9.73 4.365-9.73 9.73 0 5.366 4.365 9.73 9.73 9.73 2.034 0 3.923-.627 5.487-1.698l6.238 6.238 2.354-2.354zm-20.955-11.916c0-3.792 3.085-6.877 6.877-6.877s6.877 3.085 6.877 6.877-3.085 6.877-6.877 6.877c-3.793 0-6.877-3.085-6.877-6.877z"/></svg>
-            <svg className={`close-icon ${(searchIsOpen) ? "" : "hidden"}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M23.954 21.03l-9.184-9.095 9.092-9.174-2.832-2.807-9.09 9.179-9.176-9.088-2.81 2.81 9.186 9.105-9.095 9.184 2.81 2.81 9.112-9.192 9.18 9.1z"/></svg>
+          <button className={`toggle-search-btn ${animation} ${(searchIsOpen) ? "opened" : ""}`} onClick={toggleSearch}>
+            <svg className={`search-icon ${(showCrossIcon) ? "hidden" : ""}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M23.809 21.646l-6.205-6.205c1.167-1.605 1.857-3.579 1.857-5.711 0-5.365-4.365-9.73-9.731-9.73-5.365 0-9.73 4.365-9.73 9.73 0 5.366 4.365 9.73 9.73 9.73 2.034 0 3.923-.627 5.487-1.698l6.238 6.238 2.354-2.354zm-20.955-11.916c0-3.792 3.085-6.877 6.877-6.877s6.877 3.085 6.877 6.877-3.085 6.877-6.877 6.877c-3.793 0-6.877-3.085-6.877-6.877z"/></svg>
+            <svg className={`close-icon ${(showCrossIcon) ? "" : "hidden"}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M23.954 21.03l-9.184-9.095 9.092-9.174-2.832-2.807-9.09 9.179-9.176-9.088-2.81 2.81 9.186 9.105-9.095 9.184 2.81 2.81 9.112-9.192 9.18 9.1z"/></svg>
           </button>
           <button className={`burger-btn ${(menuIsOpen) ? "opened" : ""}`} onClick={toggleMobileMenu} aria-label="afficher le menu">
             <span></span><span></span><span></span><span></span><span></span><span></span>
@@ -35,7 +41,7 @@ const Layout = ({ location, children }) => {
           </div>
           <div className={`search-form-wrapper ${(searchIsOpen) ? "opened" : ""}`}>
             <form className="search-form">
-              <button className="search-btn" type="submit">
+              <button className={`search-btn ${animation}`} type="submit" disabled={!searchIsOpen}>
                 <svg className="search-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M23.809 21.646l-6.205-6.205c1.167-1.605 1.857-3.579 1.857-5.711 0-5.365-4.365-9.73-9.731-9.73-5.365 0-9.73 4.365-9.73 9.73 0 5.366 4.365 9.73 9.73 9.73 2.034 0 3.923-.627 5.487-1.698l6.238 6.238 2.354-2.354zm-20.955-11.916c0-3.792 3.085-6.877 6.877-6.877s6.877 3.085 6.877 6.877-3.085 6.877-6.877 6.877c-3.793 0-6.877-3.085-6.877-6.877z"/></svg>
               </button>
               <input id="search-input" type="search" placeholder="Rechercher"></input>
